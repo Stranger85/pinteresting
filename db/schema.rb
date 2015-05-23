c266ed991ad1c8377b619f68540e9c3e4c277ef9
Nikola Nikolov <nikolov.ln@gmail.com>
2015-05-24 00:07:57 +0300

    cleaned up model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522224717) do
+ActiveRecord::Schema.define(version: 20150522221556) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
